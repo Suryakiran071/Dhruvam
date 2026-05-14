50e0d60436fdb5cf5504d2171d0ab7062b0be3eb
S <2487565@cognizant.com>
2026-05-14 17:11:05 +0530

    Updated Profiles

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -12,33 +12,75 @@ export class ProfilesComponent {
   bandMembers = [
     {
       name:"Adwaith",
-      role:"Vocals",
-      photo:""
+      role:"Vocal",
+      photo:"assets/Adwaith.jpg",
+      instagram:"https://www.youtube.com/"
     },
     {
       name:"Ram",
-      role:"Vocals",
-      photo:""
+      role:"Vocal",
+      photo:"assets/Ram.jpg",
+      instagram:""
     },
     {
       name:"Aravind",
       role:"Vocals",
-      photo:""
+      photo:"assets/Aravind.jpg",
+      instagram:""
     },
     {
-      name:"Adwaith",
+      name:"Sangeetha",
       role:"Vocals",
-      photo:""
+      photo:"assets/Sangeetha.jpg",
+      instagram:""
     },
     {
-      name:"Ram",
+      name:"Nandana",
       role:"Vocals",
-      photo:""
+      photo:"assets/TG.jpg",
+      instagram:""
     },
     {
-      name:"Aravind",
-      role:"Vocals",
-      photo:""
+      name:"Adharsh",
+      role:"Lead Guitar",
+      photo:"assets/Adharsh.png",
+      instagram:""
+    },
+    {
+      name:"Surya",
+      role:"Keyboard",
+      photo:"assets/Surya.jpg",
+      instagram:""
+    },
+    {
+      name:"Govind",
+      role:"Drummer",
+      photo:"assets/Govind.jpg",
+      instagram:""
+    },
+    {
+      name:"Aadi",
+      role:"Rhythm Guitar",
+      photo:"assets/Aadi.jpg",
+      instagram:""
+    },
+    {
+      name:"Aadithyan",
+      role:"Violin",
+      photo:"assets/Mundakkal.jpg",
+      instagram:""
+    },
+    {
+      name:"Saavan",
+      role:"Keyboard",
+      photo:"assets/Saavan.jpg",
+      instagram:""
+    },
+    {
+      name:"Fahad",
+      role:"Bass",
+      photo:"assets/Fahad.jpg",
+      instagram:""
     }
   ]
 }
